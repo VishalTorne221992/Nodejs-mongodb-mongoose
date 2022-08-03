dfb8efcca153415fcf9a2e4e7dfbfd352a5fcca2
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-03 23:06:53 +0530

    commit second half
    commit second half

---
+++ app.js
@@ -0,0 +1,86 @@
+// imports
+require('dotenv').config();
+const express = require('express')
+const session = require("express-session")
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const zomatoRoutes = require('./Routes/zomato')
+const paymentRoutes = require('./Routes/payments')
+const mongoose = require('mongoose')
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const passportLocalMongoose = require("passport-local-mongoose")
+const MongoStore = require('connect-mongo');
+const MongoClient = require("mongodb").MongoClient;
+
+
+const User = require('./Models/user') 
+
+// create express server
+var app = express()
+
+// add middleware before routes
+app.use(bodyParser.json())
+app.use(cors())
+
+//connect to mongoDB
+
+const uri = 'mongodb+srv://vishal_torne_22:gowHfFl8pfW83PLj@db-first-hernode.zu6btrs.mongodb.net/zomato1?retryWrites=true&w=majority';
+
+console.log(uri, "this is the uri")
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName:'zomato1'
+}
+
+mongoose.connect(uri, options).then(() => {
+    console.log('mongoose connected')
+}).catch(e => console.log(e))
+
+
+
+mongoose.connection.on('connected', () => {
+    console.log('mongoose is connected')
+})
+
+
+app.use(session({
+    secret: "this is unambigous secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 24*60*60*1000 },
+    store   : MongoStore.create({ 
+        mongoUrl: uri,
+        ttl: 14 * 24 * 60 * 60
+     })
+    
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+
+
+// middleware routes
+app.use('/zomato', zomatoRoutes)
+app.use('/payment', paymentRoutes)
+
+
+// heroku configuration
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.resolve(__dirname, "vrestaurant/build/index.html")))
+    const path = require('path')
+    console.log(path.resolve(__dirname, "vrestaurant/build/index.html"),'this is the path that needs to be resolved')
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, "vrestaurant/build/index.html"))
+    // })
+}
+
+//listen to a port
+app.listen( process.env.PORT ||5252 , () => {
+    console.log("express app is up and running on port 5252");
+    console.log(process.env.PORT,'this is the env port')
+})
+
