8c9eaf9c94aeccf70af719b10caf974ce869df0d
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-06 11:29:28 +0530

    edited store in session added client

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: { maxAge: 24*60*60*1000 },
     store: MongoStore.create({ 
-        mongoUrl: uri,
+        client: mongoose.connection.getClient(),
+        dbName:'zomato',
         ttl: 14 * 24 * 60 * 60
      })
     
