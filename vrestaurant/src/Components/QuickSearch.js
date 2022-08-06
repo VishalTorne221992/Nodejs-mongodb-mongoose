90b7807e90d03750ccf430ec417241a3e0dffb77
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-07 02:10:46 +0530

    added cors mode to fetch

--- vrestaurant/src/Components/QuickSearch.js
+++ vrestaurant/src/Components/QuickSearch.js
@@ -24,7 +24,7 @@ export default class QuickSearch extends Component {
     }
 
     componentDidMount() {
-        fetch('https://v-restaurant.herokuapp.com/zomato/mealtype', { method: 'GET', headers: {"Access-Control-Allow-Origin": "*"} })
+        fetch('https://v-restaurant.herokuapp.com/zomato/mealtype', { method: 'GET', mode: 'cors', headers: {"Access-Control-Allow-Origin": "*"} })
             .then(response => response.json())
             .then(data => this.setState({ mealtypes: data.data }))
     }
