9353ee2f4870fb9fae24f256fbd9952edc65999b
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-06 20:43:16 +0530

    added domain heroku

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ app.use(passport.session());
 
 
 // middleware routes
-app.use('/zomato', zomatoRoutes)
-app.use('/payment', paymentRoutes)
+app.use('https://v-restaurant.herokuapp.com/zomato', zomatoRoutes)
+app.use('https://v-restaurant.herokuapp.com/payment', paymentRoutes)
 
 
 // heroku configuration
