6b923b62a1a3487093f6dded776a257ef8728307
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-08 09:24:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use(cors({
     origin: 'https://v-restaurant.herokuapp.com'
 }))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //connect to mongoDB
 
 const uri = process.env.MONGO_URI;
