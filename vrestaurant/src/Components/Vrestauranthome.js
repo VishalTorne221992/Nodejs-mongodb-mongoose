5abee7ac7525aea30dd60e118cfa2a181101f369
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-12 00:25:34 +0530

    Update Vrestauranthome.js

--- vrestaurant/src/Components/Vrestauranthome.js
+++ vrestaurant/src/Components/Vrestauranthome.js
@@ -87,14 +87,13 @@ export default class Vrestauranthome extends Component {
         },
 
         content: {
-            top: '50%',
+            top: '40%',
             left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            width: '500px',
+            width: '370px',
+            height:'400px',
             marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            overflow: "hidden",
+            transform: 'translate(-60%, -60%)',
+            overflow: "hidden"
 
         }
     }
