55131366d754bc38128da6ae0107104601402f02
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-19 18:54:42 +0530

    Update LoginComponent.js

--- vrestaurant/src/Components/LoginComponent.js
+++ vrestaurant/src/Components/LoginComponent.js
@@ -13,7 +13,7 @@ export default class LoginComponent extends Component {
       <div className='px-2 py-2' style={{display:'inline-block', width:'200px', marginLeft:'-52px', position:'relative'}}>
 
           <div className='login2' style={{ display:'inline-block', fontSize:'18px'}}><Link style={{textDecoration:'none', color:'whitesmoke'}} to='#' onClick={(e) => this.props.onClickButton(e)}>Login</Link></div>
-          <div className="createacc px-2 py-2" style={{ display:'inline-block', border:'2px solid whitesmoke'}}> <Link style={{textDecoration:'none', color:'whitesmoke'}} to='#' onClick={(e) => this.props.handleRegister(e)}> Create Account </Link></div> 
+          <div className="createacc px-2 py-2" style={{ display:'inline-block', border:'2px solid white'}}> <Link style={{textDecoration:'none', color:'white'}} to='#' onClick={(e) => this.props.handleRegister(e)}> Create Account </Link></div> 
 
       </div>
       
