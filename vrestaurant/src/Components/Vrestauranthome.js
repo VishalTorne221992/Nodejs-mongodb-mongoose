b3083f099b9a4566cb8a2271f89c84c9af381069
VishalTorne221992 <43705835+VishalTorne221992@users.noreply.github.com>
2022-08-19 22:24:17 +0530

    Update Vrestauranthome.js

--- vrestaurant/src/Components/Vrestauranthome.js
+++ vrestaurant/src/Components/Vrestauranthome.js
@@ -340,7 +340,7 @@ export default class Vrestauranthome extends Component {
                             <div className='RestSearchInput'>
 
                                 <input className="icon" type="text" placeholder="  Search for restaurants" onMouseEnter={() => this.setState({ isHovering: true })}
-                                     onChange={(e) => this.handleSearchChange(e)} />
+                                     onChange={(e) => this.handleSearchChange(e)} onFocus={this.fetchRestaurants} />
 
                             </div>
 
